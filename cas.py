0479171e67a30c6c89cc2727f09995a5cf2349f2
exoplantlalala <151989961+exoplantlalala@users.noreply.github.com>
2025-02-20 10:11:25 +0800

    Add files via upload

---
+++ cas.py
@@ -0,0 +1,52 @@
+class SpatialOperation(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(dim, dim, 3, 1, 1, groups=dim),
+            nn.BatchNorm2d(dim),
+            nn.ReLU(True),
+            nn.Conv2d(dim, 1, 1, 1, 0, bias=False),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return x * self.block(x)
+
+class ChannelOperation(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Conv2d(dim, dim, 1, 1, 0, bias=False),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return x * self.block(x)
+class CAS(nn.Module):
+    """
+    改变了proj函数的输入，不对q+k卷积，而是对融合之后的结果proj
+    """
+    def __init__(self, dim=512, attn_bias=False, proj_drop=0.):
+        super().__init__()
+        self.qkv = nn.Conv2d(dim, 3 * dim, 1, stride=1, padding=0, bias=attn_bias)
+        self.oper_q = nn.Sequential(
+            SpatialOperation(dim),
+            ChannelOperation(dim),
+        )
+        self.oper_k = nn.Sequential(
+            SpatialOperation(dim),
+            ChannelOperation(dim),
+        )
+        self.dwc = nn.Conv2d(dim, dim, 3, 1, 1, groups=dim)
+
+        self.proj = nn.Conv2d(dim, dim, 3, 1, 1, groups=dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    def forward(self, x):
+        q, k, v = self.qkv(x).chunk(3, dim=1)
+        q = self.oper_q(q)
+        k = self.oper_k(k)
+        out = self.proj(self.dwc(q + k) * v)
+        out = self.proj_drop(out)
+        return out
